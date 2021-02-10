035c9858c4b898556817ed8b54e8c3b93db3a366
Tyler <Tyler@service84.io>
2021-02-10 18:57:29 -0500

    Simplify the simpleTrue and simpleFalse specifications to lambdas

--- src/main/java/io/service84/library/standardpersistence/services/SpecificationHelper.java
+++ src/main/java/io/service84/library/standardpersistence/services/SpecificationHelper.java
@@ -1,32 +1,13 @@
 package io.service84.library.standardpersistence.services;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import org.springframework.data.jpa.domain.Specification;
 
 public class SpecificationHelper {
   public static <T> Specification<T> simpleFalse() {
-    return new Specification<>() {
-      private static final long serialVersionUID = 1L;
-
-      @Override
-      public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        return builder.or();
-      }
-    };
+    return (root, query, builder) -> builder.or();
   }
 
   public static <T> Specification<T> simpleTrue() {
-    return new Specification<>() {
-      private static final long serialVersionUID = 1L;
-
-      @Override
-      public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        return builder.and();
-      }
-    };
+    return (root, query, builder) -> builder.and();
   }
 }
